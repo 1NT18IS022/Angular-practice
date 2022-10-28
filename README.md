01844b7c5187a1e4a2081b7970b62962c24846ec
1NT18IS022 <81621077+1NT18IS022@users.noreply.github.com>
2022-10-28 19:53:02 +0530

    Initial commit

---
+++ README.md
@@ -0,0 +1,3 @@
+# Angular-practice
+
+[Edit on StackBlitz ⚡️](https://stackblitz.com/edit/angular-ivy-wupffx)
---
+++ src/app/app.component.ts
@@ -0,0 +1,26 @@
+import { Component, VERSION } from '@angular/core';
+import { formControl } from '@angular/core/schematics/migrations/typed-forms/util';
+import {
+  FormControl,
+  FormGroup,
+  ReactiveFormsModule,
+  Validators,
+} from '@angular/forms';
+
+@Component({
+  selector: 'my-app',
+  templateUrl: './app.component.html',
+  styleUrls: ['./app.component.css'],
+})
+export class AppComponent {
+  form = new FormGroup({
+    username: new FormControl('', [
+      Validators.required,
+      Validators.minLength(3),
+    ]),
+    password: new FormControl(),
+  });
+  get username() {
+    return this.form.get('username');
+  }
+}
